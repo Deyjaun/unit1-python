d76e708b957112ad6c6617a5dca2265b3ca06f52
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2023-09-19 09:52:23 -0400

    Exercise

---
+++ benchmark.py
@@ -0,0 +1,29 @@
+#Exercise 1:
+print("Deyjaun Lawrence")
+
+#Exercise 2:
+favoriteShow = x
+favoriteShow = "Yugioh"
+
+#Exercise 3:
+height = 5.10
+age = 16
+print (age * height)
+
+#Exercise 4:
+print("im going to break")
+
+#Exercise 5:
+
+#Exercise 6:
+
+#Exercise 7:
+input + String ("what is your name")
+print("Hello") + string
+
+#Exercise 8:
+
+#Exercise 9:
+x = 1
+while x == 1
+print("equal")
