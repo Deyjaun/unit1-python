5df2f244de571a42f2f3673bd878a53970c15870
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2023-09-19 10:51:01 -0400

    Exercise complete

--- benchmark.py
+++ benchmark.py
@@ -2,8 +2,7 @@
 print("Deyjaun Lawrence")
 
 #Exercise 2:
-favoriteShow = x
-favoriteShow = "Yugioh"
+favoriteShow = 'Yugioh'
 
 #Exercise 3:
 height = 5.10
@@ -11,19 +10,26 @@ age = 16
 print (age * height)
 
 #Exercise 4:
-print("im going to break")
+print('im going to break')
 
 #Exercise 5:
+fav_food = ['pizza','Fried Chicken','Watermelon']
 
 #Exercise 6:
+if age >= 21 :
+    print("You can rent a car")
+else:
+    print ("your beat")
 
 #Exercise 7:
-input + String ("what is your name")
-print("Hello") + string
+name = input ("what is your name?")
+print("Hello" + name)
 
 #Exercise 8:
-
+for i in fav_food :
+    print(i)
+    
 #Exercise 9:
 x = 1
-while x == 1
-print("equal")
+while x == 40:
+    print("equal")
