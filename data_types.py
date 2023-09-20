56717fe028ee7226e00357fa4624bd200931fa5c
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2023-09-20 10:51:55 -0400

    Taks assignment

---
+++ data_types.py
@@ -0,0 +1,44 @@
+"""
+#TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+print (23.2) #used to print the float
+print(int(23.2)) #used to print the converted float into an integer
+
+"""
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+number = input ("choose any number!") #make an input so when a number it used it gets classified into the right category
+if int(number) >= 1 : #used to make sure that any number greater than or equal to one is 
+    print("positive")
+elif int(number) == 0 :
+     print("zero")
+elif int(number) < 0 :
+     print("negetive")
+"""
+
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+
+
+"""
+#TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
