6f0753664765082867abf881c538412ae1b2bc2b
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2023-09-22 10:51:55 -0400

    conditions task

---
+++ condtional_carnival.py
@@ -0,0 +1,47 @@
+'''
+TASK 1 Even or Odd
+Determine if a number is even or odd.
+'''
+Mass = int(input("enter a number"))
+Mass = % 2
+if Mass == 0:
+    print("its an even number")
+else:
+    print("its an odd number")
+'''
+TASK 2 Positive, Negative, or Zero:
+Determine if a number is positive, negative, or zero.
+
+EXTRA CREDIT: Tell the user if they did not enter a valid number
+'''
+number=int(input("What is your number?"))
+if number > 0:
+    print("positive")
+elif number < 0:
+    print("negetive")
+elif number == 0:
+    print ("zero")
+
+'''
+TASK 3: Largest of Three
+Find and print the largest of three numbers.
+'''
+a={1,2,3}
+b=max(a)
+print(b)
+'''
+TASK 4: Leap Year
+Determine if a year is a leap year or not.
+'''
+Year = int(input("Please insert your birth year"))
+if Year % 4 == 0:
+    print("Congrats,you were born on a leap year")
+else:
+    print("Sorry you were not born on a leap year")
+'''
+TASK 5: Vowel or Consonant:
+Identify if a character is a vowel or consonant.
+
+EXTRA CREDIT: Tell the user if they did not enter a valid letter
+'''
+
