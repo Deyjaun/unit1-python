6cc0fe74e00708f16a7a95ac434e30bbeff83893
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2023-10-06 10:33:20 -0400

    range

--- ranges.py
+++ ranges.py
@@ -2,31 +2,32 @@
 Exercise 1:
 Write a program to print numbers from 1 to 10 using a for loop.
 """
-for x in range (1,11,1):
-    print(x)
+for x in range (1,11,1):#sets the start number to one and counts by one till you get to ten
+    print(x)#prints numbers from 1-10
 
 """
 Exercise 2:
 Write a program to count by 10s from 900 to 1000
 """
 print("_________________")
-for y in range (900,1001,10):
-    print(y)
+for y in range (900,1001,10):#Starts at 900 and counts by 10 to 1000
+    print(y)#prints numbers 900 to 1000 counting up by 10s
 """
 Exercise 3:
 Write a program that counts form 1-100 by 9
 """
 print("________________")
-for z in range (1,101,9):
-    print(z)
+for z in range (1,101,9):#starts at 1 and counts to 100 in intervals of 9
+    print(z) #prints numbers 1-100 counting up by 9
 """
 Exercise 4:
 Write a program to calculate the sum of all numbers from 1 to 10 using a for loop.
 """
-print()
-for z in range (11):
-    z+=z
-print(z)
+print("______________________")
+sum = 0
+for z in range (1,11):
+    sum+=z
+print(sum)
 
 """
 Excercise 5:
@@ -36,9 +37,9 @@ Uncomment the following code and run it. Then answer the following:
 - Explain the iterative process that this code executes to get that output
 """
 
-# rows = 5
-# 
-# for i in range(rows):
-#     for j in range(i + 1):
-#         print('*', end=' ')
-#     print()
+rows = 5
+
+for i in range(rows):
+    for j in range(i + 1):
+        print('*', end=' ')
+    print()
