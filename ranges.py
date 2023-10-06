3407b31f9b1fc28aea558375360b4de28570afcd
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2023-10-06 10:12:22 -0400

    ranges

---
+++ ranges.py
@@ -0,0 +1,44 @@
+"""
+Exercise 1:
+Write a program to print numbers from 1 to 10 using a for loop.
+"""
+for x in range (1,11,1):
+    print(x)
+
+"""
+Exercise 2:
+Write a program to count by 10s from 900 to 1000
+"""
+print("_________________")
+for y in range (900,1001,10):
+    print(y)
+"""
+Exercise 3:
+Write a program that counts form 1-100 by 9
+"""
+print("________________")
+for z in range (1,101,9):
+    print(z)
+"""
+Exercise 4:
+Write a program to calculate the sum of all numbers from 1 to 10 using a for loop.
+"""
+print()
+for z in range (11):
+    z+=z
+print(z)
+
+"""
+Excercise 5:
+Uncomment the following code and run it. Then answer the following:
+- What is the ouput of the code?
+
+- Explain the iterative process that this code executes to get that output
+"""
+
+# rows = 5
+# 
+# for i in range(rows):
+#     for j in range(i + 1):
+#         print('*', end=' ')
+#     print()
